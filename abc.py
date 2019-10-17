01d2d4fc10d9d2547646f2c5dfa9af3f1f52d0d1
funcube <vivicd12@gmail.com>
2019-10-17 16:19:14 +0800

    listbox

--- abc.py
+++ abc.py
@@ -1,4 +1,5 @@
-# tkinter 的資料輸入然後按鍵後文件輸出。 
+# tkinter 的Listbox視窗部件（選項）
+
 
 import tkinter as tk
 from tkinter import filedialog
@@ -11,30 +12,33 @@ window.title('這個是我的第一個視窗表')
 
 window.geometry('500x300')
 
+# 在圖形介面上建立一個標籤label用以顯示並放置
+var1 = tk.StringVar()  # 建立變數，用var1用來接收滑鼠點選具體選項的內容
+l = tk.Label(window, bg='yellow', fg='black',font=('Arial', 12), width=10, textvariable=var1)
+l.pack()
 
-e1 = tk.Entry(window, show='*', font=('Arial', 14))   # 顯示成密文形式
-e2 = tk.Entry(window, show=None, font=('Arial', 14))  # 顯示成明文形式
-e1.pack()
-e2.pack()
+# 建立一個方法用於按鈕的點選事件
+def print_selection():
+    value = lb.get(lb.curselection())   # 獲取listbox(lb)當前格子選中的文字
+    var1.set(value)  # 為label設定值
 
-# 第5步，定義兩個觸發事件時的函式insert_point和insert_end（注意：因為Python的執行順序是從上往下，所以函式一定要放在按鈕的上面）
-def insert_point(): # 在滑鼠焦點處插入輸入內容
-    var1 = e1.get() +e2.get()
-    t.insert('insert', var1)
-def insert_end():   # 在文字框內容最後接著插入輸入內容
-    var2 = e1.get()
-    t.insert('end', var2)
-# 第6步，建立並放置兩個按鈕分別觸發兩種情況
-b1 = tk.Button(window, text='insert point', width=10,
-               height=2, command=insert_point)
+# 建立一個按鈕並放置，點選按鈕呼叫print_selection函式
+b1 = tk.Button(window, text='打印選擇', width=15, height=2, command=print_selection)
 b1.pack()
 
-b2 = tk.Button(window, text='insert end', width=10,
-               height=2, command=insert_end)
-b2.pack()
+# 建立Listbox併為其新增內容
+var2 = tk.StringVar()
+var2.set(('vivi','wendy','aken','4')) # 為變數var2設定值
 
-# 第7步，建立並放置一個多行文字框text用以顯示，指定height=3為文字框是三個字元高度
-t = tk.Text(window, height=3)
-t.pack()
+# 建立Listbox
+lb = tk.Listbox(window, listvariable=var2)  #將var2的值賦給Listbox
+# 建立一個list並將值迴圈新增到Listbox控制元件中
+list_items = [11,22,33,44]
+for item in list_items:
+    lb.insert('end', item)  # 從最後一個位置開始加入值
+lb.insert(1, 'first')       # 在第一個位置加入'first'字元
+lb.insert(3, 'second')      # 在第二個位置加入'second'字元
+lb.delete(2)                # 刪除第二個位置的字元
+lb.pack()
 
 window.mainloop()
